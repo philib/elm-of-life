f7a39c0f0df6a12d549037bcb92114efc58b10f0
Philip Schäfer <00philip00@googlemail.com>
2020-05-31 16:17:33 +0200

    render game as simple table

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,38 @@
 module Main exposing (..)
 
+import Html exposing (Html, div, td, text, tr)
+import List exposing (concatMap, filter, length, map, range)
 type alias Point = { x: Int, y: Int}
 type State = Alive | Dead
 type alias Cell = { state: State, point: Point }
 type alias Neighbours = List Cell
-type alias Game = List Cell
+type alias Game = List (List Cell)
+
+main = renderGame createGame
+
+renderGame: Game -> Html msg
+renderGame game = div [] (map renderRow game)
+
+renderRow: List Cell -> Html msg
+renderRow cells = tr [] (map renderCell cells)
+
+renderCell: Cell -> Html msg
+renderCell cell =
+    case cell.state of
+        Alive -> td [] [text "X"]
+        Dead -> td [] [text "O"]
+
+createGame: Game
+createGame = map (\x -> map (\y -> createCell x y) (range 0 10) ) (range 0 10)
+
+createCell: Int -> Int -> Cell
+createCell x y = {state = Alive, point= { x=x, y=y}}
 
 getNewGeneration: Game -> Game
-getNewGeneration game = List.map (\x -> getNewCellState x (getNeighbours x game)) game
+getNewGeneration game = map (\row -> getNewGenerationList game row) game
+
+getNewGenerationList: Game -> List Cell -> List Cell
+getNewGenerationList game cells = map (\x -> getNewCellState x (getNeighbours x game)) cells
 
 getNewCellState: Cell -> Neighbours -> Cell
 getNewCellState cell neighbours =
@@ -18,13 +43,13 @@ getNewCellState cell neighbours =
             else
                 { state= Alive, point= cell.point}
         Alive ->
-            if(List.length neighbours == 1 || List.length neighbours > 3) then
+            if(List.length neighbours == 1 || length neighbours > 3) then
                 { state = Dead, point = cell.point}
             else
                 cell
 
 getNeighbours: Cell -> Game -> Neighbours
-getNeighbours cell allCells = List.filter (\c -> isNeighbour cell.point c.point) allCells
+getNeighbours cell allCells = filter (\c -> isNeighbour cell.point c.point) (concatMap identity allCells)
 
 isNeighbour: Point -> Point -> Bool
 isNeighbour a b = not (isSame a b) && ( (a.y == b.y + 1 || a.y == b.y - 1 || a.y == b.y) &&(a.x == b.x + 1 || a.x == b.x - 1 || a.x == b.x) )
